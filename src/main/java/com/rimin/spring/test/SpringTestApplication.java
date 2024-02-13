dd6d74130cbb38cbfcf6111b60cb4efa8033d9ff
kwon_laptop <chris0540@naver.com>
2024-02-13 18:28:13 +0900

    [권민석]  - 오류 해결, jsp 파일 연결 필요

--- src/main/java/com/rimin/spring/test/SpringTestApplication.java
+++ src/main/java/com/rimin/spring/test/SpringTestApplication.java
@@ -1,10 +1,14 @@
 package com.rimin.spring.test;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
+
 @SpringBootApplication
+
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
